0b8d4b31f301c9951a69b22986e7dbabf0399b1d
dikkisetiawan <initialsds@gmail.com>
2023-08-14 22:35:46 +0700

    reward module update

--- src/services/point-transaction/point-transaction.module.ts
+++ src/services/point-transaction/point-transaction.module.ts
@@ -4,6 +4,7 @@ import { PointTransactionResolver } from './point-transaction.resolver';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
-  providers: [PrismaService,PointTransactionResolver, PointTransactionService]
+  providers: [PrismaService, PointTransactionResolver, PointTransactionService],
+  exports: [PointTransactionResolver],
 })
 export class PointTransactionModule {}
--- src/services/reward/reward.module.ts
+++ src/services/reward/reward.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RewardService } from './reward.service';
 import { RewardResolver } from './reward.resolver';
 import { PrismaService } from 'prisma/prisma.service';
+import { PointTransactionModule } from '../point-transaction/point-transaction.module';
 
 @Module({
-  providers: [PrismaService,RewardResolver, RewardService]
+  providers: [PrismaService, RewardResolver, RewardService],
+  imports: [PointTransactionModule],
 })
 export class RewardModule {}
