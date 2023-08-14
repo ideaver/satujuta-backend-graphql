ccb33afa28f0e31e802fba908135ac7fb9b196d7
dikkisetiawan <initialsds@gmail.com>
2023-08-14 17:11:43 +0700

    updated point transaction  resolver and service

--- src/services/point-transaction/point-transaction.resolver.ts
+++ src/services/point-transaction/point-transaction.resolver.ts
@@ -7,6 +7,7 @@ import { PointTransactionCreateArgs } from './dto/point-transaction-create-one.a
 import { PointTransactionFindManyArgs } from './dto/point-transaction-find-many.args';
 import { PointTransactionFindUniqueArgs } from './dto/point-transaction-find-one.args';
 import { PointTransactionUpdateOneArgs } from './dto/point-transaction-update-one.args';
+import { PointTransactionFindFirstArgs } from './dto/point-transaction-find-first.args';
 
 interface PointTransactionSelect {
   select: Prisma.PointTransactionSelect;
@@ -14,18 +15,23 @@ interface PointTransactionSelect {
 
 @Resolver(() => PointTransaction)
 export class PointTransactionResolver {
-  constructor(private readonly pointTransactionService: PointTransactionService) {}
+  constructor(
+    private readonly pointTransactionService: PointTransactionService,
+  ) {}
 
   @Mutation(() => PointTransaction, {
     nullable: true,
     description: 'Deskripsinya ada disini loh',
   })
   async pointTransactionCreateOne(
-    @Args('pointTransactionCreateArgs') pointTransactionCreateArgs: PointTransactionCreateArgs,
-    @Relations() relations: PointTransactionSelect
+    @Args('pointTransactionCreateArgs')
+    pointTransactionCreateArgs: PointTransactionCreateArgs,
+    @Relations() relations: PointTransactionSelect,
   ): Promise<PointTransaction | void> {
     pointTransactionCreateArgs.select = relations.select;
-    return await this.pointTransactionService.createOne(pointTransactionCreateArgs);
+    return await this.pointTransactionService.createOne(
+      pointTransactionCreateArgs,
+    );
   }
 
   @Query(() => [PointTransaction], {
@@ -33,7 +39,8 @@ export class PointTransactionResolver {
     description: 'Deskripsinya ada disini loh',
   })
   pointTransactionFindMany(
-    @Args('pointTransactionFindManyArgs') pointTransactionFindManyArgs: PointTransactionFindManyArgs,
+    @Args('pointTransactionFindManyArgs')
+    pointTransactionFindManyArgs: PointTransactionFindManyArgs,
     @Relations() relations: PointTransactionSelect,
   ) {
     //Auto implement prisma select from graphql query info
@@ -53,6 +60,22 @@ export class PointTransactionResolver {
     //Auto implement prisma select from graphql query info
     pointTransactionFindUniqueArgs.select = relations.select;
     return this.pointTransactionService.findOne(pointTransactionFindUniqueArgs);
+  }
+
+  @Query(() => PointTransaction, {
+    nullable: true,
+    description: 'Bisa juga untuk ambil balance terakhir dari point milik user',
+  })
+  pointTransactionFindFirst(
+    @Args('pointTransactionFindFirstArgs')
+    pointTransactionFindFirstArgs: PointTransactionFindFirstArgs,
+    @Relations() relations: PointTransactionSelect,
+  ) {
+    //Auto implement prisma select from graphql query info
+    pointTransactionFindFirstArgs.select = relations.select;
+    return this.pointTransactionService.findFirst(
+      pointTransactionFindFirstArgs,
+    );
   }
 
   // @Mutation(() => PointTransaction, { description: 'Deskripsinya ada disini loh' })
