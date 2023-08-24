449166a7e6cd1e332c8c27a8604684699ee1bfa7
initialds <initialsds@gmail.com>
2023-08-24 09:55:59 +0700

    updated rewardclaim module1

--- src/services/reward-claim/reward-claim.module.ts
+++ src/services/reward-claim/reward-claim.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { RewardClaimService } from './reward-claim.service';
 import { RewardClaimResolver } from './reward-claim.resolver';
 import { PrismaService } from 'prisma/prisma.service';
+import { PointTransactionModule } from '../point-transaction/point-transaction.module';
 import { RewardClaimController } from './reward-claim.controller';
 import { RewardModule } from '../reward/reward.module';
 
@@ -12,6 +13,6 @@ import { RewardModule } from '../reward/reward.module';
     RewardClaimController,
     RewardClaimService,
   ],
-  imports: [RewardModule],
+  imports: [PointTransactionModule, RewardModule],
 })
 export class RewardClaimModule {}
