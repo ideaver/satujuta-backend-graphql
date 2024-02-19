19af8223f6d4ee1fa721b07df58ae52055ad90ea
Dikki Setiawan <initialsds@gmail.com>
2024-02-19 23:00:11 +0700

    Refactor initialization service and user seed functions

--- src/services/initialization/initialization.service.ts
+++ src/services/initialization/initialization.service.ts
@@ -6,6 +6,8 @@ import { encryptUserPassword } from 'src/utils/bcrypt.function';
 import { Item } from 'src/@generated';
 import { ItemController } from '../item/item.controller';
 import { UserRole } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import { generateRandomReferralCode } from 'src/utils/generate-random.function';
 
 // initialization.service.ts
 @Injectable()
@@ -19,37 +21,118 @@ export class InitializationService {
   private readonly logger = new Logger(InitializationService.name);
 
   async initialize() {
-    const subdistricts = await this.prisma.subdistrict.findMany();
+    const subdistrictsPromise = this.prisma.subdistrict.findMany();
+    const userFindManyPromise = this.prisma.user.findMany();
+    const itemsPromise = this.itemController.findMany({});
+
+    const [subdistricts, userFindMany, items] = await Promise.all([
+      subdistrictsPromise,
+      userFindManyPromise,
+      itemsPromise,
+    ]);
 
     if (subdistricts.length <= 0) {
       await populateProvinceCityDistricSubdistric();
     }
 
-    const items: Item[] | void = await this.itemController.findMany({});
+    const createAdminPromise =
+      userFindMany.length <= 0 ? this.createAdmin() : Promise.resolve();
+    const createSuperUserPromise =
+      userFindMany.length <= 0 ? this.createSuperUser() : Promise.resolve();
+    const createItemPromise =
+      items.length <= 0 ? this.createItem() : Promise.resolve();
 
-    if (items.length <= 0) {
-      await this.prisma.item
-        .createMany({
-          data: [
-            {
-              name: 'Paket PREMIUM SatuJuta Membership',
-              description: 'Berlisensi PT. SatuJuta Kampung Inggris',
-              price: 1000000,
-              cost: 300000,
-              userRole: UserRole.MEMBER,
+    await Promise.all([
+      createAdminPromise,
+      createSuperUserPromise,
+      createItemPromise,
+    ]);
+  }
+
+  private async createItem() {
+    await this.prisma.item
+      .createMany({
+        data: [
+          {
+            name: 'Paket PREMIUM SatuJuta Membership',
+            description: 'Berlisensi PT. SatuJuta Kampung Inggris',
+            price: 1000000,
+            cost: 300000,
+            userRole: UserRole.MEMBER,
+          },
+          {
+            name: 'Registrasi Siswa',
+            description: '',
+            price: 1000000,
+            cost: 300000,
+            userRole: UserRole.STUDENT,
+          },
+        ],
+      })
+      .then(() => {
+        this.logger.log('Item data has been created');
+      });
+  }
+
+  async createSuperUser() {
+    await this.prisma.user
+      .create({
+        data: {
+          firstName: 'Super',
+          lastName: 'User',
+          email: 'superuser@satujuta.com',
+          password: await encryptUserPassword('123456'),
+          theme: 'LIGHT',
+          userRole: 'SUPERUSER',
+          userType: 'OTHER',
+          whatsappNumber: '6289683743880',
+          avatarUrl: faker.image.avatar(),
+          referralCode: generateRandomReferralCode(),
+          address: {
+            create: {
+              name: 'www.alvamind.com',
+              subdistrict: {
+                connect: {
+                  id: 1,
+                },
+              },
             },
-            {
-              name: 'Registrasi Siswa',
-              description: '',
-              price: 1000000,
-              cost: 300000,
-              userRole: UserRole.STUDENT,
+          },
+        },
+      })
+      .then(() => {
+        this.logger.log('Super User has been created');
+      });
+  }
+
+  async createAdmin() {
+    await this.prisma.user
+      .create({
+        data: {
+          firstName: 'Admin',
+          lastName: 'User',
+          email: 'admin@satujuta.com',
+          password: await encryptUserPassword('123456'),
+          theme: 'LIGHT',
+          userRole: 'ADMIN',
+          userType: 'OTHER',
+          whatsappNumber: '62839683743880',
+          avatarUrl: faker.image.avatar(),
+          referralCode: generateRandomReferralCode(),
+          address: {
+            create: {
+              name: 'www.alvamind.com',
+              subdistrict: {
+                connect: {
+                  id: 1,
+                },
+              },
             },
-          ],
-        })
-        .then(() => {
-          this.logger.log('Item data has been created');
-        });
-    }
+          },
+        },
+      })
+      .then(() => {
+        this.logger.log('Admin User has been created');
+      });
   }
 }
